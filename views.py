edc03f2e3d5e290476d7496d9897629b6440539f
Ruben Albertassi <albertassi88@gmail.com>
2020-07-24 15:27:21 -0300

    Django MySQL

---
+++ views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import Produto
+from .forms import ProdutoModelForm
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
+def index(request):
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+def produto(request):
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar o produto.')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
+
+
